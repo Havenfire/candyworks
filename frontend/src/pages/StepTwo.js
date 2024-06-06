89d9db886458bbd033013a033be6d766e7e570ef
Havenfire <bsliu24@gmail.com>
2024-06-06 16:10:47 -0400

    step two completed, needs to pass values

--- frontend/src/pages/StepTwo.js
+++ frontend/src/pages/StepTwo.js
@@ -4,79 +4,22 @@ import { useNavigate } from 'react-router-dom';
 import './StepTwo.css';
 import '../global.css';
 import { COLORS } from '../values/colors';
-import img1 from '../images/midgate_mudball.png';
-import img2 from '../images/shade_shore_sugarworms.png';
-import img3 from '../images/skywrath_squawksicles.png';
-import img4 from '../images/goldlake_glitterfish.png';
-import img5 from '../images/oglodi_trail_jerky.png';
 
 const StepTwo = () => {
   const navigate = useNavigate();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedItems, setSelectedItems] = useState([]);
-  const menuRef = useRef(null);
+  const [value, setValue] = useState(20);
 
-  // Create an array of objects representing the candies
-  const candies = [
-    { id: 1, image: img1, name: "Midgate Mudball" },
-    { id: 2, image: img2, name: "Shade Shore Sugarworms" },
-    { id: 3, image: img3, name: "Skywrath Squawksicles" },
-    { id: 4, image: img4, name: "Goldlake Glitterfish" },
-    { id: 5, image: img5, name: "Oglodi Trail Jerky" },
-  ];
-
-  // Sort the candies array by ID
-  candies.sort((a, b) => a.id - b.id);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const addItem = (index) => {
-    const newItem = candies[index];
-    const newSelectedItems = [...selectedItems, newItem].sort((a, b) => a.id - b.id);
-    if (selectedItems.length < 30) {
-      setSelectedItems(newSelectedItems);
-    }
-    setIsMenuOpen(false); // Close menu after selection
-  };
-
-  const removeItem = (index) => {
-    const newItems = selectedItems.filter((_, i) => i !== index);
-    setSelectedItems(newItems);
-  };
-
-  const handleClickOutside = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      setIsMenuOpen(false);
+  const increment = () => {
+    if (value < 30) {
+      setValue(value + 1);
     }
   };
 
-  const handleKeyPress = (event) => {
-    if (event.key >= '1' && event.key <= '5') {
-      const index = parseInt(event.key) - 1;
-      addItem(index);
+  const decrement = () => {
+    if (value > 20) {
+      setValue(value - 1);
     }
   };
-
-  useEffect(() => {
-    if (isMenuOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [isMenuOpen]);
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyPress);
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  });
-
   const goToNext = () => {
     navigate('/step3');
   };
@@ -99,46 +42,35 @@ const StepTwo = () => {
         </div>
         <div className="click-the-to-select-a-candy-parent">
           <div style={{ color: COLORS.grey, fontSize: 14 }} className="click-the">
-            Click the + to select a candy, or tap the matching shortcut key.  
+            Select how many slots you have unlocked for your candy inventory. (Minimum 20, maximum 30)
           </div>
         </div>
         <div className="frame-wrapper">
-          <div className="selected-items-wrapper">
-            {Array.from({ length: 3 }).map((_, row) => {
-              const startIndex = row * 10;
-              const endIndex = startIndex + 10;
-              if (startIndex < selectedItems.length) { // Only create the row if items exist for it
-                return (
-                  <div key={row} className="row-container">
-                    {selectedItems.slice(startIndex, endIndex).map((item, index) => (
-                      <div key={index} className="selected-item-container">
-                        <img src={item.image} alt={item.name} className="selected-item-icon" />
-                        <button className="selected-items-remove" onClick={() => removeItem(startIndex + index)}>X</button>
-                      </div>
-                    ))}
-                  </div>
-                );
-              }
-              return null;
-            })}
-            <div className="frame-container" ref={menuRef}>
-              <div className="wrapper">
-                <button className="menu-button" onClick={toggleMenu}>+</button>
+          <div className="frame-container">
 
-                {isMenuOpen && (
-                  <div className="menu">
-                    {candies.map((candy, index) => (
-                      <button key={index} className="menu-item" onClick={() => addItem(index)}>
-                        <img src={candy.image} alt={`Option ${candy.id}`} />
-                        <p>{candy.name}</p>
-                      </button>
-                    ))}
-                  </div>
-                )}
-                
-              </div>
+            <div className="wrapper">
+              <button
+                className={`menu-button ${value === 20 ? 'faded' : ''}`}
+                onClick={decrement}
+                disabled={value === 20}
+              >
+                -
+              </button>
+            </div>
+            <div className="text-wrapper">
+              <input type="text" className="text-box" value={value} defaultValue={20} />
+            </div>
+            <div className="wrapper">
+              <button
+                className={`menu-button ${value === 30 ? 'faded' : ''}`}
+                onClick={increment}
+                disabled={value === 30}
+              >
+                +
+              </button>
             </div>
           </div>
+
         </div>
 
         <div className="of-4-parent">
